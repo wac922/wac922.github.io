95b0234c1b0cdf46bca4c2a52c6e705f0ccd5528
RyanChen <wac922@gmail.com>
2017-05-12 10:54:55 +0800

    tanfenpages

---
+++ web01/php/tanfen-order-goods.php
@@ -0,0 +1,8 @@
+<option value="">-請選擇-</option>
+<option value="b-01">紫心番薯麵包</option>
+<option value="b-02">葡萄丹麥捲</option>
+<option value="b-03">猶太辮子麵包</option>
+<option value="c-01">檸檬生乳酪蛋糕</option>
+<option value="c-02">抹茶紅豆月餅</option>
+<option value="c-03">美式曲奇</option>
+<option value="c-04">義式硬脆餅</option>
